77fae4145235e64e5fc181a51148a716b2a22779
natavel <natacha_belladonny@mail.ru>
2020-11-29 22:52:42 +0300

    Небольшие добавления во фрагментах

--- app/src/main/java/ru/cosiness/myapplication/FragmentMoviesList.kt
+++ app/src/main/java/ru/cosiness/myapplication/FragmentMoviesList.kt
@@ -2,30 +2,22 @@ package ru.cosiness.myapplication
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 
 /**
  * Created by Natasha on 27.11.2020.
  * tg: natavel
  */
 class FragmentMoviesList:AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_movies_list)
-    }
 
-    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
-        super.onSaveInstanceState(outState, outPersistentState)
-        Log.d(TAG, "$LOG_PREFIX::onSaveInstanceState")
-    }
+    fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_movies_list, container, false)
 
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        Log.d(TAG, "$LOG_PREFIX::onRestoreInstanceState")
-    }
-
-    companion object {
-        private const val TAG = "rere"
-        private const val LOG_PREFIX = "fdfd"
-    }
 }
